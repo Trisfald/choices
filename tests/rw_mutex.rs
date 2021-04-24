1840ac477eb469d1203e7ffe15538bfaff3e27ea
Trisfald <trisfald@gmail.com>
2021-04-24 12:59:36 +0000

    test for RwLock

---
+++ tests/rw_mutex.rs
@@ -0,0 +1,45 @@
+use choices::Choices;
+use lazy_static::lazy_static;
+use std::future::Future;
+use std::sync::{Arc, RwLock};
+use tokio::runtime::Runtime;
+use util::*;
+
+#[derive(Choices)]
+struct Config {
+    debug: bool,
+}
+
+#[tokio::test]
+async fn get_field() {
+    let port = get_free_port!();
+
+    let rt = Runtime::new().unwrap();
+    rt.spawn(async move {
+        lazy_static! {
+            static ref CONFIG: Arc<RwLock<Config>> = Arc::new(RwLock::new(Config { debug: true }));
+        }
+        CONFIG.run((std::net::Ipv4Addr::LOCALHOST, port)).await
+    });
+
+    check_get_text!(port, "config/debug", "true");
+
+    rt.shutdown_background();
+}
+
+#[tokio::test]
+async fn put_field() {
+    let port = get_free_port!();
+
+    let rt = Runtime::new().unwrap();
+    rt.spawn(async move {
+        lazy_static! {
+            static ref CONFIG: Arc<RwLock<Config>> = Arc::new(RwLock::new(Config { debug: true }));
+        }
+        CONFIG.run((std::net::Ipv4Addr::LOCALHOST, port)).await
+    });
+
+    check_put_field_text!(port, debug, "false", 200, "false");
+
+    rt.shutdown_background();
+}
